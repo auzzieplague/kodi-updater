d36b63ba09d824515fdbee2a22005a7190ee2762
Michael Whinfrey <auzzieplague@gmail.com>
2016-12-08 22:57:05 +1100

    cleaned up code, added versions count for better debugging. spaced out GUI a bit more.

---
+++ kodiupdates/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace kodiupdates
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.filelist = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.errorlist = new System.Windows.Forms.ListBox();
+            this.versions = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(95, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Latest Versions";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(326, 50);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(300, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 177);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(286, 21);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // filelist
+            // 
+            this.filelist.FormattingEnabled = true;
+            this.filelist.Location = new System.Drawing.Point(15, 50);
+            this.filelist.Name = "filelist";
+            this.filelist.Size = new System.Drawing.Size(216, 82);
+            this.filelist.TabIndex = 4;
+            this.filelist.SelectedIndexChanged += new System.EventHandler(this.filelist_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ready ...";
+            // 
+            // errorlist
+            // 
+            this.errorlist.FormattingEnabled = true;
+            this.errorlist.Location = new System.Drawing.Point(11, 264);
+            this.errorlist.Name = "errorlist";
+            this.errorlist.Size = new System.Drawing.Size(615, 69);
+            this.errorlist.TabIndex = 6;
+            // 
+            // versions
+            // 
+            this.versions.FormattingEnabled = true;
+            this.versions.Location = new System.Drawing.Point(237, 50);
+            this.versions.Name = "versions";
+            this.versions.Size = new System.Drawing.Size(64, 82);
+            this.versions.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Filenames";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(237, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Versions";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 247);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.versions);
+            this.Controls.Add(this.errorlist);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.filelist);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Kodi Addon Updates";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListBox filelist;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox errorlist;
+        private System.Windows.Forms.ListBox versions;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ kodiupdates/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace kodiupdates
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static void UnZip(string zipFile, string folderPath)
+        {
+            if (!File.Exists(zipFile))
+                throw new FileNotFoundException();
+
+            if (!Directory.Exists(folderPath))
+                Directory.CreateDirectory(folderPath);
+
+            Shell32.Shell objShell = new Shell32.Shell();
+            Shell32.Folder destinationFolder = objShell.NameSpace(folderPath);
+            Shell32.Folder sourceFile = objShell.NameSpace(zipFile);
+
+            try
+            {
+                foreach (var file in sourceFile.Items())
+                {
+                    destinationFolder.CopyHere(file, 4 | 16);
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private string GetLatestVersion(string inputFileUrl)
+        {
+            try
+            {
+                WebClient client = new WebClient();
+                Uri uri = new Uri(inputFileUrl);
+                client.DownloadFile(uri, "scanfile.txt");
+                string[] lines = System.IO.File.ReadAllLines(@"scanfile.txt");
+                string filename = ""; // lines[lines.Length - 3].Split('"')[1];
+                int versions = 0;
+                foreach (string line in lines)
+                {
+                    if (line.Contains(".zip"))
+                    {
+                        filename = line.Split('"')[1];
+                        versions++;
+                    }
+                }
+                //MessageBox.Show(filename);
+                uri = new Uri(inputFileUrl+ filename);
+                client.DownloadFile(uri, @"downloads\"+ filename);
+                return filename+":"+versions.ToString();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("cannot process "+ inputFileUrl, "Problem reading web url", MessageBoxButtons.OK);
+                errorlist.Items.Add("cannot process " + inputFileUrl);
+                return "";
+            }
+        }
+
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //get correct downloads file
+            string updatefilename = "downloads.txt";
+            if (!File.Exists("downloads.txt"))
+            {
+                updatefilename = "kodiupdates.txt";
+                WebClient client = new WebClient();
+                Uri uri = new Uri(@"http://www.michaelwhinfrey.com/projects/kodiupdates.txt");
+                try {
+                    client.DownloadFile(uri, updatefilename);
+                }
+                catch
+                {
+       
+                }
+
+                if (!File.Exists(updatefilename))
+                {
+                    try
+                    {
+                        uri = new Uri(@"http://www.computertown.com.au/webupdate/kodiupdates.txt");
+                        client.DownloadFile(uri, updatefilename);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("update file not found, check internet connection", "cant read file", MessageBoxButtons.OK);
+                    }
+                }
+            }
+
+            //read info into listbox
+
+            try {
+
+                string[] lines = System.IO.File.ReadAllLines(updatefilename);
+                foreach (string line in lines)
+                {
+                    listBox1.Items.Add(line);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("filenot found, try running as administrator", "cant read file", MessageBoxButtons.OK);
+            }
+
+            if (!Directory.Exists("downloads"))
+            {
+                Directory.CreateDirectory("downloads");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = listBox1.Items.Count;
+            progressBar1.Value = 0;
+            label2.Text = "Downloading ...";
+            //check repos for latest versions
+            foreach (string item in listBox1.Items)
+            {
+                progressBar1.Value ++;
+                string filename=GetLatestVersion(item.ToString());
+                if (filename != "")
+                {
+                    filelist.Items.Add(filename.Split(':')[0]);
+                    versions.Items.Add(filename.Split(':')[1]);
+                }
+                this.Update();
+            }
+
+
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = filelist.Items.Count;
+            progressBar1.Value = 0;
+            label2.Text = "Extracting ...";
+            
+
+            string outputpath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\kodi\addons";
+            if (!Directory.Exists(outputpath))
+            {
+                outputpath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\kodi\userdata\addon_data\";
+            }
+
+            foreach (string item in filelist.Items)
+            {
+                progressBar1.Value++;
+                try {
+                    UnZip(AppDomain.CurrentDomain.BaseDirectory + @"downloads\" + item.ToString(), outputpath);
+                }
+                catch
+                {
+                    label2.Text = "Extraction failed!";
+                    progressBar1.Value = 0;
+                    MessageBox.Show("was unable to extract " +item.ToString()+" to "+outputpath, "Having Trouble!", MessageBoxButtons.OK);
+                    return;
+                }
+                this.Update();
+            }
+
+            //Windows Start -type %APPDATA%\kodi\userdata - press < Enter >
+            label2.Text = "Complete!";
+
+            if (errorlist.Items.Count > 0)
+            {
+                this.Height = 285;
+                MessageBox.Show("updates complete, some errors encountered!", "All done", MessageBoxButtons.OK);
+            }
+            else  MessageBox.Show("updates complete", "All done", MessageBoxButtons.OK);
+
+        }
+
+        private void filelist_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            versions.SelectedIndex = filelist.SelectedIndex;
+        }
+    }
+}
